a4680610890131a9a4710ecdbfa6563900e59c16
Niko7even <pozzernico@gmail.com>
2024-08-05 02:24:24 -0300

    en proceso del video 265

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,76 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { countries } from "../../data/countries";
+import styles from "./Form.module.css"
+import { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
+
+
+export default function form() {
+
+  // con el generic "<SearchType>" le asignamos el tipo de dato
+  //a el state search y setSearch
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const [alert, setAlert] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> |
+    ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+        ...search,
+        [e.target.name] : e.target.value
+    })// tomamos una copia de search y modificamos el atributo q queremos
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if(Object.values(search).includes('')){
+        setAlert("Todos los campos son obligatorios")
+        return // con este return hacemos que no se ejecuten las siguientes lineas de codigo
+    }
+  }
+
+  return (
+    <form className={styles.form}
+            onSubmit={handleSubmit}
+        >
+        
+        {alert && <Alert/>}
+        <div className={styles.field}>
+            <label htmlFor="city">Ciudad:</label>
+            <input 
+                id="city"
+                type="text" 
+                name="city"
+                placeholder="Ciudad"
+                value={search.city}
+                onChange={handleChange}
+            />
+        </div>
+
+        <div className={styles.field}>
+            <label htmlFor="country">Pais:</label>
+            <select
+                id="country"
+                value={search.country}
+                name="country"
+                onChange={handleChange}
+            >
+                <option value="">-- Seleccione un Pais ---</option>
+                {countries.map( country => (//Esto hace un select y recorre la carpeta data
+                    <option
+                        key={country.code}
+                        value={country.code}
+                    >{country.name}</option>
+                ))}
+            </select>
+        </div>
+
+        <input className={styles.submit} type="submit" value='Consultar Clima' />
+    </form>
+  )
+}
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+
+
+export interface SearchType {
+    city: string;
+    country: string;
+}
+
+export interface Country {
+    code: string
+    name: string
+}
